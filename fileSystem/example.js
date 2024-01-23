0bc5c753f68670e66ba78d92f674718451a1e8f9
tjess25 <88819417+tjess25@users.noreply.github.com>
2024-01-22 22:14:33 -0600

    clase asincronia

--- fileSystem/example.js
+++ fileSystem/example.js
@@ -1,9 +1,8 @@
 const fs = require('fs')
 
-const path = process.argv[2]
+//const path = process.argv[2]
 
-
-fs.writeFile('./example.txt', 'Hola Koders!!!!!', (err) => {
+/*fs.writeFile('./example.txt', 'Hola Koders!!!!!', (err) => {
     if (err) throw new Error("No se puede crear el archivo") 
     // if (err) {
     //    throw err
@@ -25,7 +24,7 @@ fs.writeFile('./example.txt', 'Hola Koders!!!!!', (err) => {
             })
         })
     })
-})
+})*/
 
 /*fs.readFile('./example.txt', 'utf-8', (err, data) => {
     if (err) throw new Error("No se puede leer el archivo")
@@ -55,3 +54,8 @@ fs.unlink('./example.txt', (err) => {
  * 6) imprime en consola "Archivo creado"
  * 7) lee el archivo --> se genera error
  */
+
+fs.mkdir('./newFolder', (err) => {
+    if (err) throw err
+    console.log("Folfer creado correctamente");
+})
---
+++ promesas.js
@@ -0,0 +1,101 @@
+const fs = require('fs')
+/*function getUsers() {
+    setTimeout(() => {
+        return users
+    }, 3000);
+}*/
+
+/*const getUsers = new Promise((resolve, reject) => {
+    const users = [
+        {
+            "user":"users1",
+            "password":"nduiwh68tgyu",
+            "email":"mail@mail.com",
+        },
+        {
+            "user":"users2",
+            "password":"nduiwh68tgyu",
+            "email":"mail2@mail.com",
+        },
+        {
+            "user":"users3",
+            "password":"nduiwh68tgyu",
+            "email":"mail3@mail.com",
+        }  
+    ]
+    setTimeout(() => {
+        resolve(users) 
+    }, 3000);    
+})
+
+getUsers.then((users) => {
+    console.log(users)
+}).catch((err) => {
+    console.error(err)
+}).finally(() => {
+    console.log("Estoy dentro de la promesa")
+})
+
+console.log("Estoy a fuera de la promesa")*/
+
+/*let mkdir = new Promise((resolve, reject) => {
+    fs.mkdir('./newFolder', (err) => {
+        if (err) reject(err) 
+        resolve("Folfer creado correctamente");
+    })
+})
+
+let createHTML = new Promise((resolve, reject) => {
+    fs.writeFile("index.html", "", (err)=> {
+        if (err) reject(err) 
+        resolve("index creado correctamente");
+    })
+})*/
+
+/*createDir.then((msg) => {
+    console.log(msg)
+}).catch((err) => {
+    throw err
+}).finally(createHTML)*/
+
+async function getUsers() {
+    const users = [
+        {
+            "user":"users1",
+            "password":"nduiwh68tgyu",
+            "email":"mail@mail.com",
+        },
+        {
+            "user":"users2",
+            "password":"nduiwh68tgyu",
+            "email":"mail2@mail.com",
+        },
+        {
+            "user":"users3",
+            "password":"nduiwh68tgyu",
+            "email":"mail3@mail.com",
+        }  
+    ]
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(users)  
+        }, 3000); 
+    })
+    
+}
+
+function main() {
+    async function logUsers() {
+        let users = await getUsers()
+        console.log(users)
+    }
+    logUsers()
+}
+main()
+
+
+
+
+
+
+
